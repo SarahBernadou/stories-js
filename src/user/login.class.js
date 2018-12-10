78bfd6c14e2c491813170fd0b84a1db7c5538f65
Sarah <bernadou.s@gmail.com>
2018-12-10 16:11:18 +0100

    Effacement du formulaire si erreur et refactorage du login

--- src/user/login.class.js
+++ src/user/login.class.js
@@ -6,29 +6,60 @@ class Login {
         // Modifier le titre de la page
         $('#main-title').html('Identifiez-vous') //html fait reference a h1 dans index
 
+        // Je defini mes attributs
+        this.login = $('[name="loginField"]');
+        this.password = $('[name="passwordField"]');
+
         //Definition du listener sur le formulaire
         this.formListener(); //this correspond a l'objet concerné de la classe Login
+        this.submitListener();
     }
+
     /**
      * formListener Gestionnaire d'évènements sur le formulaire de Login
      * @param void
      * @return void
      */
     formListener() {
+        let login = this.login;
+        let password = this.password;
+
         $('#loginForm').on( //Je veux écouter tous les changements au lever d'une touche ('keyup') sur le formulaire Login
             'keyup',
-            //Callback : Fonction appelée si l'évènement defini survient, ici, lors d'un changement
+            //Callback : Fonction appelée si l'évènement defini survient, ici, lors d'un changement, et le this n'est pas accepté dans un callback
             function (event) {
-                //Verifier le contenu des deux champs
-                const login = $('[name=loginField]');
-                const password = $('[name=passwordField]');
-
+                
                 //Est ce que les deux champs sont remplis et le login a plus ou 5 caractères
                 if (login.val().length >= 5 && password.val() !== '') {
                     $('#btnLogin').removeAttr('disabled');
                 } else {
                     // Non, ça ne le fait pas tout seul, il faut le lui dire
                     $('#btnLogin').attr('disabled', 'disabled');
+                }
+            }
+        );
+    }
+    submitListener() {
+        let login = this.login;
+        let password = this.password;
+
+        $('#loginForm').on(
+            'submit',
+            function (event) {
+                event.preventDefault(); //Empeche l'action par defaut (ici l'envoie du formulaire)
+                const user = new User();
+                user.setUserName(login.val());
+                user.setPassword(password.val());
+
+                // Gere l'authentification
+                if (user.authentification()) {
+                    console.log('Ok, ça roule');
+                } else {
+                    console.log('Nop, t\'as pas l\'droit !');
+                    login.val('');
+                    password.val('');
+
+                    $('btnLogin').attr('disabled', 'disabled');
                 }
             }
         );
---
+++ src/user/user.class.js
@@ -0,0 +1,35 @@
+/**
+ * @name User
+ * @description Service de gestion des utilisateurs
+ * @author Aelion
+ * @version 1.0.0
+ */
+
+class User {
+    constructor() { }
+
+    /** Definit le Username de l'utilisateur
+     * @param {*} userName
+     */
+    setUserName(userName) {
+        this.userName = userName;
+    }
+
+    /** Definit le mot de passe de l'utilisateur
+    * @param {*} password
+    */
+    setPassword(password) {
+        this.password = password;
+    }
+
+    /** Identifie l'utilisateur à partir d'un login et d'un mot de passe
+     * @returns boolean
+     */
+    authentification() {
+        if (this.userName === 'sbernadou' && this.password === 'sbe') {
+            this.group = 'Administrateur';
+            return true;
+        }
+        return false;
+    }
+}
